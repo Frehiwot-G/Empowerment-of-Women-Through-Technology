53480dbedfa71df13ec13f704b82c3b6c07a2638
Frehiwot-G <frehiwotgem@gmail.com>
2023-04-08 09:15:18 -0700

    google translator

--- Empowerment/settings.py
+++ Empowerment/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'google_translate',
     'AUTH',
     'women_in_tech',
     'Job',
