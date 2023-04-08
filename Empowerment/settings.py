dfc048187a759247fad590462a810c69790eb67c
Frehiwot-G <frehiwotgem@gmail.com>
2023-04-08 07:01:00 -0700

    modify env

--- Empowerment/settings.py
+++ Empowerment/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cyn^aq&$27^&g2=+j%=)6#sl*k6ot%&pnx9al*kxeu6^135$ps'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =  config('DEBUG')
 
 ALLOWED_HOSTS = []
 
